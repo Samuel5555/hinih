456e7bdd6da409a60d6534443991253d0fceb632
SammyStax <57230694+Samuel5555@users.noreply.github.com>
2025-03-29 16:42:49 -0700

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,37 @@
+
+
+// /*var x = 10;
+// var a = x + 40;
+// document.write(a);
+// var video, htmlTracks;
+// var trackStatusesDiv;
+
+// window.onload = function(){
+//     video = document.querySelector("#myVideo");
+//     trackStatusesDiv = document.querySelector("#trackStatusesDiv");
+
+//     htmlTracks = document.querySelectorAll("track");
+
+//     displayTrackStatuses(htmlTracks);
+// };
+// function displayTrackStatuses(htmlTracks){
+//     for(var i = 0; i < htmlTracks.length; i++) {
+//         var currentHtmlTrack = htmlTracks[i];
+//         var label = "<li>label = " + currentHtmlTrack.label + "</li>";
+//         var kind = "<li>kind = " + currentHtmlTrack.kind + "</li>";
+//         var lang = "<li>lang = " + currentHtmlTrack.srclang + "</li>";
+//         var readyState = "<li>readyState = " + currentHtmlTrack.readyState + "</li>";
+        
+//         trackStatusesDiv.innerHTML += "<li><b>Track:" + i + ":</b></li>"
+//         + "<ul>" + label + kind + lang + readyState + "</ul>";
+//     }
+// }
+// */
+// $(document).ready(function(){
+// 	$(".dropdown, .btn-group").hover(function(){
+// 		var dropdownMenu = $(this).children(".dropdown-menu");
+// 		if(dropdownMenu.is(":visible")){
+// 			dropdownMenu.parent().toggleClass("open");
+// 		}
+// 	});
+// });		
